5b062d6277275a6480d78bed1df786d1c7173a9d
chenshuxian1997 <ChenAmy1997@outlook.com>
2018-01-07 22:01:22 +0800

    Add files via upload

--- winmaker.rb
+++ winmaker.rb
@@ -1,32 +1,39 @@
 require 'sketchup.rb'
 require 'offset.rb'
-def find_png(file_name="") #查找png图片，获取全路径名
-	path=Sketchup.find_support_file file_name,"plugins/image"
-	return path
+#查找png图片，获取全路径名
+def find_png(file_name="") 
+  path=Sketchup.find_support_file file_name,"plugins/image"
+  return path
 end
 module Win
 #选择面
-	def win.sel
-	selection=Sketchup.active.model_selection
-	#分别成组
-	selection.each do |face|
-		face=selection.add_group
-	end
+	def Win.sel
+  	selection=Sketchup.active_model.selection
+		#分别成组
+  	selection.each do |face|
+			face=selection.add_group
+  	end
 	end
 	#弹窗（窗框宽度，厚度，玻璃厚度）
-	def win.form
+  def Win.form
 		prompts=["窗框厚度","窗框厚度","玻璃厚度"]
-		defults=["","",""]
-		results=inputbox prompts,defults,"参数"	
+		val=["","",""]
+		results=UI.inputbox prompts,val,"参数"	
+		
+
 	end
 end
+Win
 #添加菜单
-pmenu=UI.menu("Plugins") #"扩展程序"菜单栏
-submenu=pmenu.add_submenu("窗户") #添加子菜单
-submenu.add_item("rubyconsole"){win}
+
+#"扩展程序"菜单栏
+pmenu=UI.menu("Plugins") 
+#添加子菜单
+submenu=pmenu.add_submenu("窗户") 
+submenu.add_item("窗户"){Win}
 #添加工具栏
 toolbar = UI::Toolbar.new "Windows"
-cmd = UI::Command.new("Windows"){win}
+cmd = UI::Command.new("Windows"){Win}
 cmd.small_icon =find_png "win.png"
 cmd.large_icon =find_png "win.png"
 cmd.tooltip = "Windows"
